a39f779c35df4af301b0fb9dad0679eaf07bfe11
frendly <www.zpdf@mail.ru>
2019-07-21 12:30:29 +0300

    add gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,15 @@ const {gulp, src, dest, watch, series, parallel} = require('gulp');
 const include = require('gulp-include');
 const prettyHtml = require('gulp-pretty-html');
 const del = require('del');
+const browserSync = require('browser-sync');
 
 const paths = {
   input: 'src/',
   output: 'dist/',
   html: {
     input: 'src/**/*.html',
-  }
+  },
+  reload: './dist/'
 }
 
 function cleanDist (done) {
@@ -36,8 +38,42 @@ function gulpPrettyHtml() {
           .pipe(dest(paths.input))
 };
 
+// Watch for changes to the src directory
+function startServer (done) {
+
+  // Initialize BrowserSync
+  browserSync.init({
+    server: {
+      baseDir: paths.reload
+    }
+  });
+
+  // Signal completion
+  done();
+};
+
+// Reload the browser when files change
+function reloadBrowser (done) {
+  browserSync.reload();
+  done();
+};
+
+// Watch for changes
+function watchSource (done) {
+  watch(paths.input, series(exports.default, reloadBrowser));
+  done();
+};
+
+
 exports.pretty = gulpPrettyHtml;
 exports.default = series(
   cleanDist,
   htmlIncludes,
+);
+
+// Watch and reload
+exports.watch = series(
+  exports.default,
+  startServer,
+  watchSource
 );
