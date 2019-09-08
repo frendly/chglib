9ef0c77bd72ff1fab36e319396768b2de80433ca
frendly <www.zpdf@mail.ru>
2019-09-08 20:58:17 +0300

    add new holidays

--- src/assets/js/holidays.js
+++ src/assets/js/holidays.js
@@ -4,14 +4,13 @@ import { extendMoment } from 'moment-range';
 
 const moment = extendMoment(Moment);
 
-// import { range, isRange } from 'moment-range';
-
 import { getCurrentYear } from "./utils";
 
-const getFullDate = (date) => moment(date, 'DD-MM').format();
+const getFullDate = (date) => moment(date, 'DD-MM');
 
 function showHolidaysInfo() {
-  var url = '/assets/data/holidays.json';
+  const url = '/assets/data/holidays.json';
+  const today = moment().endOf('day');
 
   $.getJSON(url).done(function(data) {
     var event = data[0];
@@ -19,11 +18,12 @@ function showHolidaysInfo() {
     
     const start = getFullDate(startDate);
     const end = getFullDate(endDate);
-    const dateRange = moment.range(start, end);
-    // moment.isRange(range)
+    const range = moment.range(start, end);
+    const isShowing = range.contains(today);
     
-    // console.log(start, end);
-    console.log(dateRange, moment.isRange(range));
+    // console.log(start, end, today);
+    // console.log(dateRange);
+    console.log(isShowing);
 
     var text = replaceText(event.text);
     $('<section>').html(text).prependTo('.last-news');
