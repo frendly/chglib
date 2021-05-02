3eb3edecdfc21d9d4a6b1d91af3a8b6c21f5950c
Popov Ivan <www.zpdf@mail.ru>
2021-05-02 14:16:28 +0300

    check build mode in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,46 +4,49 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  target: ["web"],
-  entry: {
-    index: [
-      "./src/assets/js/index.js",
-      "./src/assets/styles/index.scss",
-    ],
-  },
-  output: {
-    // filename: '[name].js',
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist/assets'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          { loader: "css-loader" },
-          { loader: "sass-loader" }
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.ya?ml$/,
-        type: 'json',
-        use: 'yaml-loader'
-      }
-    ]
-  },
-  plugins: [
+module.exports = (env, options) => {
+  const isProduction = options.mode === 'production';
+
+  return {
+    target: ["web"],
+    entry: {
+      index: [
+        "./src/assets/js/index.js",
+        "./src/assets/styles/index.scss",
+      ],
+    },
+    output: {
+      filename: isProduction ? '[name].[contenthash].js' : '[name].js',
+      path: path.resolve(__dirname, 'dist/assets'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(css|scss)$/,
+          use: [
+            { loader: MiniCssExtractPlugin.loader },
+            { loader: "css-loader" },
+            { loader: "sass-loader" }
+          ]
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.ya?ml$/,
+          type: 'json',
+          use: 'yaml-loader'
+        }
+      ]
+    },
+    plugins: [
       new CleanWebpackPlugin(),
-    new WebpackManifestPlugin({ publicPath: "/assets/" }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-    }),
-  ],
+      new WebpackManifestPlugin({ publicPath: "/assets/" }),
+      new MiniCssExtractPlugin({
+        filename: isProduction ? '[name].[contenthash].css' : '[name].css',
+      }),
+    ],
+  };
 };
