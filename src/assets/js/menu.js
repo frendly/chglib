57cfbf96e2016e1b688eb1bbfc14aba2184ab3a0
Popov Ivan <www.zpdf@mail.ru>
2022-07-10 16:48:15 +0300

    set menu active link by 11ty

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -6,13 +6,3 @@ export const menuMobile = () => {
       menuClasses.toggle("menu-list--open");
   });
 }
-
-// Add css class to active menu
-export const activeMenuItem = () => {
-  const path = window.location.pathname.split('/')[1];
-  const activeMenuItem = document.querySelector(`.menu-list__link[href*='${path}']`);
-
-  if (activeMenuItem) {
-    activeMenuItem.addClass('menu-list__link--active');
-  }
-}
