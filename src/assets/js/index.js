8bcd24d79ec2a27c2d13ea5a9b2645a668e2f7e1
Ivan Popov <www.zpdf@mail.ru>
2022-11-13 00:43:17 +0300

    update resbnc a2 (#57)

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,8 +2,10 @@ import holidays from './holidays';
 import analytics from './analytics';
 import { menuMobile } from './menu';
 import { openLinksInPortal } from './openLinksInPortal';
+
 import elcatToggleList from './pages/elcat';
 import getLastNews from './pages/home';
+import { resbncBbTable } from './pages/resbnc';
 
 import { mutationObserver, targetBlank, setCurrentYear } from './utils'
 
@@ -18,6 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
   analytics();
   elcatToggleList();
   getLastNews();
+  resbncBbTable();
 
   // observer
   mutationObserver(elcatToggleList);
---
+++ src/assets/js/pages/resbnc.js
@@ -0,0 +1,16 @@
+export const resbncBbTable = () => {
+  const table = document.querySelector('.js-responsive-table');
+
+  if (!table) {
+    return;
+  }
+
+  const tableTitles = table.querySelectorAll('th');
+  const tableCols = table.querySelectorAll('td');
+
+  // массив заголовков таблицы
+  const titles = Array.from(tableTitles).map(node => node.textContent);
+
+  // добавляем data атрибут с текстом заголовка, для отображения в мобильной версии таблицы
+  Array.from(tableCols).map((col, index) => col.dataset.title = titles[index % 4]);
+}
