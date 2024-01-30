c5dfcb8103df587123df9446521a36ec409f9005
Ivan Popov <ikpopov@avito.ru>
2024-01-30 16:43:11 +0300

    add files mask

--- src/assets/js/utils/targetBlank.js
+++ src/assets/js/utils/targetBlank.js
@@ -1,12 +1,12 @@
 /**
  * Добавляем target='_blank'
  * - для внешних ссылок
- * - для внутренних ссылок на файлы pdf, jpg
+ * - для внутренних ссылок на файлы pdf, jpg, doc, docx,ppt
  */
 export const targetBlank = () => {
   const urls = document.querySelectorAll("a");
   const host = location.host.replace("www.", "");
-  const pattern = new RegExp('^(https?:\\/\\/)?' + host + '(?!.*\\.(pdf|jpg))', 'i');
+  const pattern = new RegExp('^(https?:\\/\\/)?' + host + '(?!.*\\.(pdf|jpg|doc|docx|ppt))', 'i');
 
   const externalAndResourceUrls = Array.from(urls).filter((url) => !url.href.match(pattern));
   externalAndResourceUrls.forEach(link => link.setAttribute('target', '_blank'));
