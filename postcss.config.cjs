ff1c255b3a707dc8e482f64cd14c23a00c0d584b
Ivan Popov <www.zpdf@mail.ru>
2025-06-03 17:42:34 +0300

    refactor: SCSS -> postCSS (#116)

---
+++ postcss.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('postcss-custom-media')(),
+    require('postcss-preset-env')({
+      stage: 1, // Поддержка черновиков CSS (0-4, где 4 — стабильные)
+    })
+  ]
+};
