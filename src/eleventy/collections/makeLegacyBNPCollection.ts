c77e3a29c3f91590eb127f90e53cb109cf94dfa4
Ivan Popov <ikpopov@avito.ru>
2026-01-28 19:17:13 +0300

    fix/legacy-bnp-use-autocollection

---
+++ src/eleventy/collections/makeLegacyBNPCollection.ts
@@ -0,0 +1,37 @@
+import path from 'node:path';
+import type { EleventyCollection, EleventyCollectionItem } from '@/types/eleventy';
+
+/**
+ * Создает коллекцию для LEGACY_BNP на основе папок
+ * Например legacyBnpByYear = { 2025: [{}], 2024: [{}], ... }
+ * @param collection - Коллекция Eleventy
+ * @param folderName - Имя папки для создания коллекции
+ * @returns Объект с годами в качестве ключей и массивами постов в качестве значений
+ */
+export const makeLegacyBNPCollection = (
+  collection: EleventyCollection,
+  folderName: string
+): Record<string, EleventyCollectionItem[]> => {
+  const files = collection.getFilteredByGlob(`./pages/${folderName}/**/*.html`);
+  return files.reduce(
+    (years, post) => {
+      /**
+       * В коллекцию попадают только файлы с маской 'bnp*.html'
+       * - Извлекаем год из пути к папке
+       * - Сортируем файлы по имени (bnp01, bnp02, ...)
+       */
+      const fileName = path.basename(post.filePathStem);
+      if (!fileName.startsWith('bnp')) {
+        return years;
+      }
+
+      const year = path.dirname(post.inputPath).split('/').pop() || '';
+      if (!years[year]) years[year] = [];
+
+      /** добавляем в начало для обратного порядка (новые первыми) */
+      years[year].unshift(post);
+      return years;
+    },
+    {} as Record<string, EleventyCollectionItem[]>
+  );
+};
