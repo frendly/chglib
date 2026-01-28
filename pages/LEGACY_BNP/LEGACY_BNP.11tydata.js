7e473e4637fc2da7c26f7d8d618cb3e9a56b5264
Ivan Popov <ikpopov@avito.ru>
2026-01-28 21:24:34 +0300

    fix/templateEngineOverride

--- pages/LEGACY_BNP/LEGACY_BNP.11tydata.js
+++ pages/LEGACY_BNP/LEGACY_BNP.11tydata.js
@@ -1,4 +1,11 @@
 export default {
+  /**
+   * Обрабатывать HTML файлы как markdown, затем как Nunjucks
+   * Это позволяет использовать markdown-форматирование (например, **bold**)
+   * внутри HTML файлов
+   */
+  templateEngineOverride: 'md,njk',
+
   permalink: (data) => {
     return `${data.page.filePathStem}.html`;
   },
